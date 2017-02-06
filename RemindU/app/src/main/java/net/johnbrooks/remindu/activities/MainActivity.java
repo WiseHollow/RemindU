3e42cfed0c6f795fcf64aea3ec242fbb12414be8
John <ieatlotz@gmail.com>
2017-02-06 15:06:46 -0500

    Refactored PagerAdapter to use custom adapter we made. MainActivity.java has a static getter for its instance.

--- RemindU/app/src/main/java/net/johnbrooks/remindu/activities/MainActivity.java
+++ RemindU/app/src/main/java/net/johnbrooks/remindu/activities/MainActivity.java
@@ -2,22 +2,24 @@ package net.johnbrooks.remindu.activities;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import net.johnbrooks.remindu.R;
+import net.johnbrooks.remindu.util.PagerAdapter;
 
 public class MainActivity extends FragmentActivity
 {
-    private ViewPager viewPager;
+    private static MainActivity mainActivity;
+    public static MainActivity GetInstance() { return mainActivity; }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager) findViewById(R.id.activity_main_pager);
-        PagerAdapter pagerAdapter = new net.johnbrooks.remindu.util.PagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.activity_main_pager);
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
+        mainActivity = this;
     }
 }
